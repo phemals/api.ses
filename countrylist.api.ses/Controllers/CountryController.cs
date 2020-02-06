4a2440349414de39b481fc2798abc38191c10a18
Hemal Patel <phemals@gmail.com>
2020-02-06 11:53:56 -0500

    initial commit

---
+++ countrylist.api.ses/Controllers/CountryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace countrylist.api.ses.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountryController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public CountryController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            logger.Log(LogLevel.Information, "Test message");
+        }
+
+        [HttpGet]
+        public IEnumerable<Country> Get()
+        {
+            using (StreamReader r = new StreamReader("/Users/Hemal/Projects/api.ses/countrylist.api.ses/Data/CountryList.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Country> items = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Country>>(json);
+                return items;
+            }
+        }
+    }
+}
---
+++ countrylist.api.ses/Country.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace countrylist.api.ses
+{
+    public class Country
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
